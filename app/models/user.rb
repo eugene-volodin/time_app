b8294eb4c99aa542d10f52ab094a915fa316a950
Eugene <last.eldar@gmail.com>
2017-10-04 00:01:12 +0300

    added md5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :s_email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX }  
+
+  def get_md5(str)
+    Digest::MD5.hexdigest(str)
+  end
+
 end
